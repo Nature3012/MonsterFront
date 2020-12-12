271319ffcb901f922e095e4d781bdb71bf27cfea
watakou <kaizokuou@icloud.com>
2020-12-12 19:35:43 +0900

    Resultmanagerの実装、途中

---
+++ Assets/Script/ResultManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] GameObject item1;//レア報酬
+    [SerializeField] int coin;
+}
