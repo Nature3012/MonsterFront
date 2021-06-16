0d47dc2650543849a4f9234d11dfd124c16fed01
watakou <kaizokuou@icloud.com>
2021-06-16 17:18:18 +0900

    BaceEnemyの実装及びStage1の敵の実装 playerControllは少しだけ実装してしまった

--- Assets/Sc/BaceEnemy.cs
+++ Assets/Sc/BaceEnemy.cs
@@ -14,6 +14,10 @@ public class BaceEnemy : MonoBehaviour
     public Transform enemyTransform { private set; get; }//敵のTransform
     public Transform targetTransform { private set; get; }//プレイヤーのTransform
     public Animator enemyAnimator { private set; get; }//敵のアニメーター
+    public NavMeshAgent navMeshAgent { private set; get; }//
+    private bool isSet = false; //Setしたかどうか 
+    private float currentSeconds = 0;//Freeze時開始からの秒数
+    private float attackE;
 
     public virtual void SetEnemyName(string s) {enemyName = s;}
     public virtual string GetEnemyName() {return enemyName;}
@@ -38,11 +42,13 @@ public class BaceEnemy : MonoBehaviour
         IDOL,
         LOOKTARGET,
         ATTACK,
-        FREEZE
+        FREEZE,
+        Die
     }
+    ENEMYSTATE enemyState = ENEMYSTATE.IDOL;
 
-    public virtual void SetEnemy(string name, float hp, float speed, 
-        float power, float freeze, Animator animator, Rigidbody rigidbody, Transform enemyTrans, Transform targetTrans)
+    public void SetEnemy(string name, float hp, float speed, 
+        float power, float freeze, Animator animator, Rigidbody rigidbody, Transform enemyTrans, Transform targetTrans,NavMeshAgent navMesh)
     {
         enemyName = name;
         enemyHp = hp;
@@ -53,10 +59,137 @@ public class BaceEnemy : MonoBehaviour
         enemyTransform = enemyTransform;
         targetTransform = targetTrans;
         enemyAnimator = animator;
+        navMeshAgent = navMesh;
+        isSet = true;
     }
 
     private void Update()
     {
-        
+        if (isSet)
+        {
+            switch (enemyState)
+            {
+                case ENEMYSTATE.IDOL:
+                    enemyState = ENEMYSTATE.LOOKTARGET;
+                    break;
+                case ENEMYSTATE.LOOKTARGET:
+                    Vector3 playerPos = targetTransform.position;
+                    playerPos.y = this.transform.position.y;
+                    this.transform.LookAt(playerPos);
+                    enemyState = ENEMYSTATE.ATTACK;
+                    break;
+                case ENEMYSTATE.ATTACK:
+                    float aida = (enemyTransform.position - targetTransform.position).sqrMagnitude;
+                    float attckdis = (targetTransform.gameObject.GetComponent<CapsuleCollider>().radius + this.GetComponent<CapsuleCollider>().radius + attackE) + 
+                        (targetTransform.gameObject.GetComponent<CapsuleCollider>().radius + this.GetComponent<CapsuleCollider>().radius + attackE);
+                    int attackCase = RandomAttack();
+                    switch (attackCase)
+                    {
+                        case 0:
+                            Attack1();
+                            break;
+                        case 1:
+                            Attack2(aida,attckdis);
+                            break;
+                        case 2:
+                            Attack3(aida,attckdis);
+                            break;
+                        case 3:
+                            Attack4(aida,attckdis);
+                            break;
+                        case 4:
+                            Attack5(aida,attckdis);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case ENEMYSTATE.FREEZE:
+                    Freeze();
+                    break;
+                case ENEMYSTATE.Die:
+                    Die();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private int RandomAttack() 
+    {
+        int attack = Random.Range(0,5);
+        return attack;
+    }
+
+    public virtual void Attack1() 
+    {
+        enemyState = ENEMYSTATE.FREEZE;
+    }
+
+    public virtual void Attack2(float aida,float attackdis) 
+    {
+        enemyState = ENEMYSTATE.FREEZE;
+    }
+
+    public virtual void Attack3(float aida, float attackdis) 
+    {
+        enemyState = ENEMYSTATE.FREEZE;
+    }
+
+    public virtual void Attack4(float aida, float attackdis) 
+    {
+        enemyState = ENEMYSTATE.FREEZE;
+    }
+
+    public virtual void Attack5(float aida, float attackdis) 
+    {
+        enemyState = ENEMYSTATE.FREEZE;
+    }
+
+    private void Freeze() 
+    {
+        currentSeconds += Time.deltaTime;
+        if (enemyFreeze <= currentSeconds)
+        {
+            enemyState = ENEMYSTATE.IDOL;
+            currentSeconds = 0;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            enemyHp -= collision.gameObject.GetComponent<Bullet>().Po();
+            if (enemyHp <= 0)
+            {
+                enemyState = ENEMYSTATE.Die;
+            }
+        }
+    }
+
+    public void BeamDameze(float dameze) 
+    {
+        enemyHp -= dameze;
+        if (enemyHp <= 0)
+        {
+            enemyState = ENEMYSTATE.Die;
+        }
+    }
+
+    public void Die() 
+    {
+        this.tag = "Untagged";
+    }
+
+    public void AnimatorEnd(string s)
+    {
+        float animInfo = enemyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        if (animInfo < 1.0f) { }
+        else
+        {
+            enemyState = ENEMYSTATE.FREEZE;
+        }
     }
 }
---
+++ Assets/Sc/EnemyStage1.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyStage1 : BaceEnemy
+{
+    [SerializeField] private string enemyThisName;
+    [SerializeField] private float enemyThisHp;
+    [SerializeField] private float speed;
+    [SerializeField] private float power;
+    [SerializeField] private float freeze;
+    [SerializeField] private GameObject attack1;
+    [SerializeField] private Transform attackSporn;
+
+    private void Awake()
+    {
+        base.SetEnemy(enemyThisName,enemyThisHp,speed,power,freeze,GetComponent<Animator>(), GetComponent<Rigidbody>(), this.gameObject.transform, 
+            GameObject.FindGameObjectWithTag("Player").transform,GetComponent<NavMeshAgent>());
+    }
+
+    public override void Attack1()
+    {
+        enemyAnimator.Play("Attack1");
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetTransform.position - transform.position), 0.5f);
+        Instantiate(attack1, attackSporn.position, Quaternion.identity); 
+        base.Attack1();
+    }
+
+    public override void Attack2(float aida, float attackdis)
+    {
+        if (attackdis >= aida / 400)
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            enemyAnimator.Play("Attack2");
+            AnimatorEnd("Attack2");
+            enemyAnimator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(targetTransform.position);
+            enemyAnimator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+        }
+        base.Attack2(aida, attackdis);
+    }
+
+    public override void Attack3(float aida, float attackdis)
+    {
+        if (attackdis >= aida / 400)
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            enemyAnimator.Play("Attack3");
+            AnimatorEnd("Attack3");
+            enemyAnimator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(targetTransform.position);
+            enemyAnimator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+        }
+        base.Attack3(aida, attackdis);
+    }
+
+    public override void Attack4(float aida, float attackdis)
+    {
+        if (attackdis >= aida / 400)
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            enemyAnimator.Play("Attack4");
+            AnimatorEnd("Attack4");
+            enemyAnimator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(targetTransform.position);
+            enemyAnimator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+
+        }
+        base.Attack4(aida, attackdis);
+    }
+
+    public override void Attack5(float aida, float attackdis)
+    {
+        if (attackdis >= aida / 400)
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            enemyAnimator.Play("Attack5");
+            AnimatorEnd("Attack5");
+            enemyAnimator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(targetTransform.position);
+            enemyAnimator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+
+        }
+        base.Attack5(aida, attackdis);
+    }
+}
